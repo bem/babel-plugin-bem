3b7a94fb567c9298c034e1ebda8af22c68551b38
Sergey Berezhnoy <veged@yandex-team.ru>
2015-08-11 17:03:33 +0300

    all cases covered

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import { types as t } from 'babel-core';
 import path from 'path';
+import bemNaming from 'bem-naming';
 
 export default function({ Plugin, types: t }) {
-    console.log('!!!!!');
-    var isBemModule = false,
+    var fileBemItem = null,
+        isBemModule = false,
         bemModules = {},
-        deps = [];
+        deps = [],
+        prevDecl = null,
+        asyncProvide = null;
 
     function transform(program, deps, file) {
         var body = program.body,
-            defineArgs = [t.literal(path.basename(file.filename, '.js'))],
-            declParams = [t.identifier('provide')];
-
-        deps = deps.map(d => t.literal(d));
+            defineArgs = [t.literal(bemNaming.stringify({...fileBemItem, modName : undefined }))],
+            declParams = [t.identifier(asyncProvide || 'provide')];
 
         if(deps.length) {
+            deps = deps.map(d => {
+                declParams.push(t.identifier(d[0]));
+                return t.literal(d[1]);
+            });
             defineArgs.push(t.arrayExpression(deps));
         }
+
+        prevDecl && declParams.push(t.identifier(prevDecl));
 
         defineArgs.push(t.functionExpression(null, declParams, t.blockStatement(body)));
 
@@ -37,44 +44,66 @@ export default function({ Plugin, types: t }) {
     return new Plugin('bem', {
         visitor : {
             Program : {
+                enter(node, parent, scope, file) {
+                    fileBemItem = bemNaming.parse(path.basename(file.opts.filename, '.js'));
+                },
+
                 exit(node, parent, scope, file) {
                     node.body = file.dynamicImports.concat(node.body);
 
                     bemModules[file.opts.filename] && transform(node, deps, file.opts);
+                    fileBemItem = null;
                     deps = [];
+                    prevDecl = null;
+                    asyncProvide = null;
                     //this.unshiftContainer('body', t.expressionStatement(t.literal('use helloworld')));
                 }
             },
 
             ImportDeclaration(node, parent, scope, file) {
-                var importPath = node.source.value;
+                var importPath = node.source.value,
+                    filename = file.opts.filename;
                 if(importPath.indexOf('bem-source:') === 0) {
                     importPath = node.source.value = path.resolve(
-                        path.dirname(file.opts.filename),
+                        path.dirname(filename),
                         importPath.replace(/^bem-source:/, '')
                     );
                     bemModules[importPath] = true;
                 } else if(importPath.indexOf('bem:') === 0) {
-                    deps.push(importPath.replace(/^bem:/, ''));
+                    var localName = getSingleImportDefaultSpecifier(node).local.name,
+                        importName = importPath.replace(/^bem:/, ''),
+                        importBemItem = bemNaming.parse(importName);
+
+                    if(importBemItem.modName) throw Error(`Importing of modifier modules (${importName}) is not supported.`);
+
+                    if(fileBemItem.block === importBemItem.block && fileBemItem.elem === importBemItem.elem) {
+                        prevDecl = localName;
+                    } else {
+                        deps.push([localName, importName]);
+                    }
+
+                    return [];
+                } else if(importPath === 'ym:provide') {
+                    asyncProvide = getSingleImportDefaultSpecifier(node).local.name;
                     return [];
                 }
-
-                //this.replaceWith(
-                //    t.callExpression(
-                //        t.memberExpression(
-                //            t.identifier('modules'),
-                //            t.identifier('define'),
-                //            false
-                //        ),
-                //        [
-                //
-                //        ]));
             },
 
             ExportDefaultDeclaration(node, parent, scope, file) {
-                return t.callExpression(t.identifier('provide'), [node.declaration])
+                if(bemModules[file.opts.filename])
+                    return t.callExpression(t.identifier('provide'), [node.declaration])
             }
         }
     });
 
+}
+
+function getSingleImportDefaultSpecifier(node) {
+    var specifiers = node.specifiers,
+        specifier = node.specifiers[0];
+
+    if(specifiers.length === 1 && specifier.type === 'ImportDefaultSpecifier')
+        return specifier;
+
+    throw Error(`Only single import default specifier is supported: ${node.source.value}.`);
 }
